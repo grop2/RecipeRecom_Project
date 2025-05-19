c123304632d0eccaa2a5814fe5ec62c18cfc3d1e
Rawanalim <rawanalim05@gmail.com>
2025-05-19 22:12:56 +0000

    Final change for script 3

--- scripts/getInstr.sh
+++ scripts/getInstr.sh
@@ -3,5 +3,5 @@
 echo -n "Enter part or full recipe name: "
 read recipe
 
-# Go through the recipes and print name + instructions for matching ones
+# Go through the recipes and print name + instructions for the matching ones
 grep -i -A 4 "name:.*$recipe" recipes.txt | grep -i -E "^name:|^instructions:"
